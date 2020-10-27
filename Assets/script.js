4f04190e40248ca08cc04fe7498099623f4836cf
Sam Fan <samisfan@msn.com>
2020-10-27 15:17:35 -0600

    update js

--- Assets/script.js
+++ Assets/script.js
@@ -6,7 +6,7 @@
 // 5-day forecast for the city w/weather condition icon, temp and humidity
 
 
-// default API key 0da5ed5abba6e7099462925a45deecdd
+
 
 
 // api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
