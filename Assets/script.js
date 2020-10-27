9f44ee556eca6313ff407235c80764bf532068a6
Sam Fan <samisfan@msn.com>
2020-10-27 16:58:59 -0600

    html and css mostly done, will need to tweak a few classes once js is started

--- Assets/script.js
+++ Assets/script.js
@@ -4,9 +4,10 @@
 // City name and date with current weather icon (cloud, sun, etc...)
 // Temp, humidity, wind speed, UV index with a color icon background indicating scale of favorable, moderate, severe
 // 5-day forecast for the city w/weather condition icon, temp and humidity
-
-
-
-
-
 // api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+
+// target the search input
+// fetch data for 5-day forecast, return into 5 cards and .city
+// create recent items list, with a href links going back to the previous search item for each item
+// localStorage will store recent city, state, country names in an index
+// 
