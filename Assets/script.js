28a68b25cfa6e3d6ecd3022443fcc30384f6201f
Sam Fan <samisfan@msn.com>
2020-10-26 18:00:26 -0600

    First commit, file structure, basic html and css started, pseudo code started

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+// Weather dashboard
+// search field - fetch query
+// list of recent city searches below, should be active links that can be clicked, taking to the search query results for that city
+// City name and date with current weather icon (cloud, sun, etc...)
+// Temp, humidity, wind speed, UV index with a color icon background indicating scale of favorable, moderate, severe
+// 5-day forecast for the city w/weather condition icon, temp and humidity
