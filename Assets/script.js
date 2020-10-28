213bc6f1a445abe859547803a78e3b51b66d9b96
Sam Fan <samisfan@msn.com>
2020-10-28 14:37:59 -0600

    started js, populating the 5-day

--- Assets/script.js
+++ Assets/script.js
@@ -11,3 +11,28 @@
 // create recent items list, with a href links going back to the previous search item for each item
 // localStorage will store recent city, state, country names in an index
 // 
+
+var requestUrl = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}';
+
+var responseText = document.getElementById('response-text');
+
+function getApi(requestUrl) {
+  fetch(requestUrl)
+    .then(function (response) {})
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+getApi(requestUrl);
+var cardTarget = document.getElementById("#cardstart")
+var dayDiv1 = document.createElement("div");
+var dayId = document.createAttribute("id", "date[i]");
+var dayStyle = document.setAttribute("style", "width:12rem");
+var dayDiv2 = document.createElement("div");
+var fiveDay = ["dayOne", "dayTwo", "dayThree", "dayFour", "dayFive"];
+
+// creates the 5-day card html ==============================
+function fiveDay () {
+    
+    for (i = 0; i < fiveDay.length; i++) {
