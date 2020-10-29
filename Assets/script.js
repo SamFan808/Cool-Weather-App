703c45f2795fa9f47e9f2b237a91c5da1c466c4e
Sam Fan <samisfan@msn.com>
2020-10-28 18:03:15 -0600

    working on js, getting cards to show up, but not playing nicely with row

--- Assets/script.js
+++ Assets/script.js
@@ -12,27 +12,45 @@
 // localStorage will store recent city, state, country names in an index
 // 
 
-var requestUrl = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}';
+// var requestUrl = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}';
 
-var responseText = document.getElementById('response-text');
+// var responseText = document.getElementById('response-text');
 
-function getApi(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {})
-    .then(function (data) {
-      console.log(data);
-    });
-}
+// function getApi(requestUrl) {
+//   fetch(requestUrl)
+//     .then(function (response) {})
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
 
-getApi(requestUrl);
-var cardTarget = document.getElementById("#cardstart")
-var dayDiv1 = document.createElement("div");
-var dayId = document.createAttribute("id", "date[i]");
-var dayStyle = document.setAttribute("style", "width:12rem");
-var dayDiv2 = document.createElement("div");
-var fiveDay = ["dayOne", "dayTwo", "dayThree", "dayFour", "dayFive"];
+// getApi(requestUrl);
+var cardTarget = document.getElementById("cardstart");
+var dayArray = ["day1", "day2", "day3", "day4", "day5"];
+
+// https://stackoverflow.com/a/15643868/14244725 - uhh, put the variables inside the loop/function
+function dayCard () {
+  var dayS1 = document.createElement("section");
+  var dayS2 = document.createElement("section");
+  var dayH5 = document.createElement("h5");
+  var dayP1 = document.createElement("p");
+  cardTarget.appendChild(dayS1);
+  dayS1.className ="card bg-primary";
+  dayS1.style = "width:12rem";
+  dayS1.dataset.day = "day"+[i+1];
+  dayS1.appendChild(dayS2);
+  dayS2.className ="card-body";
+  dayS2.appendChild(dayH5);
+  dayH5.className = "card-title";
+  dayS2.appendChild(dayP1);
+  dayP1.className = "card-text";
+ }
 
 // creates the 5-day card html ==============================
 function fiveDay () {
-    
-    for (i = 0; i < fiveDay.length; i++) {
+    for (i = 0; i < dayArray.length; i++) {
+    dayCard();
+    }
+  };
+
+fiveDay();
