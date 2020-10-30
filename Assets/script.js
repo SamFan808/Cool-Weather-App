0042581c7e831dd0fda734cfe0413f03636216fb
Sam Fan <samisfan@msn.com>
2020-10-30 14:16:46 -0600

    js revised, fetch calls functioning and populating, need to do 5-day fetch call and verify date

--- Assets/script.js
+++ Assets/script.js
@@ -24,74 +24,91 @@ var cityRecents =[];
 var fetchButton = document.querySelector(".btn");
 var cityB1 = document.getElementsByClassName("city");
 
-fetchButton.addEventListener('click', function(event) {
-  event.preventDefault(); {
+function getData () {
+  // clear the currentWeather and the daycards
+  $('section').remove();
+
   var cityInput = document.querySelector("input");
   var cityText = cityInput.value;
   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ cityText +"&appid=0d2d646f1fc53c15f97082e153457db8&units=imperial";
+
   if (cityText === "") {
     return;
   } else {
-    // clear the currentWeather and the daycards
-    fetch(requestUrl) 
-      .then(function (response) {
-        return response.json();
-      })
+    // current weather fetch request ===============================
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
       .then(function (data) { 
-        cityB1.textContent = data.name;
-        console.log(data);
-        console.log(cityInput);
-        console.log(cityText);
-        console.log(requestUrl);
-        console.log(data.main.temp);
+        // this builds the current weather card
+        var cityBlock = document.createElement("section");
+        var city = document.createElement("section");
+        var date = document.createElement("span");
+        var temp = document.createElement("section");
+        var wind = document.createElement("section");
+        var uv = document.createElement("section");
+        boxTarget.after(cityBlock);
+        cityBlock.className = "col-md-8";
+        cityBlock.id = "cityblock";
+        cityBlock.appendChild(city).className = "city";
+        cityBlock.appendChild(span).className = "date"
+        cityBlock.appendChild(temp).className = "temperature";
+        cityBlock.appendChild(wind).className = "windspeed";
+        cityBlock.appendChild(uv).className = "uvindex";
+        city.textContent = "City: " + data.name;
+        date.textContent = "(" + data.date + ")";
+        temp.textContent = "Temperature: " + data.main.temp + "F";
+        wind.textContent = "Windspeed: " + data.wind.speed + "MPH";
+        // UV index fetch request ================================
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        var uvRequest = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=0d2d646f1fc53c15f97082e153457db8&"
+        fetch(uvRequest)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (dataUV) { 
+          uv.textContent = "UV Index " + dataUV.value;
+          // button color based on some # < or > some uv index #
+        });
+        // https://stackoverflow.com/a/15643868/14244725 - uhh, put the variables inside the loop/function
+        // 5-day fetch request ====================================
+        var fiveRequest = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=0d2d646f1fc53c15f97082e153457db8&"
+        fetch(fiveRequest)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (dataFive) {
+        
+        
+        
+        // creates the 5-day card html
+        var dayTitle = document.querySelector("#daytitle");
+        dayTitle.textContent = "5-Day Forecast:";
+        for (i = 0; i < dayArray.length; i++) {
+          var dayS1 = document.createElement("section");
+          var dayS2 = document.createElement("section");
+          var dayH5 = document.createElement("h5");
+          var dayP1 = document.createElement("p");
+          cardTarget.after(dayS1);
+          dayS1.className ="card bg-primary";
+          dayS1.style = "width:10rem";
+          dayS1.dataset.day = "day"+[i+1];
+          dayS1.appendChild(dayS2).className ="card-body";
+          dayS2.appendChild(dayH5).className = "card-title";
+          dayS2.appendChild(dayP1).className = "card-text";
+        }
       })
-    }
   }
-  currentWeather();
-  dayCard();
-});
+}
+
 // this function should build a list based on recent city searches
 function recents () {
   var liN1 = document.createElement("li");
   inputTarget.appendChild(liN1).className = "list-group-item py-2";
 }
-// this function builds the current weather card
-function currentWeather () {
-  var cityBlock = document.createElement("article"); 
-  var city = document.createElement("article");
-  var temp = document.createElement("article");
-  var wind = document.createElement("article");
-  var uv = document.createElement("article");
-  boxTarget.after(cityBlock);
-  cityBlock.className = "col-md-8";
-  cityBlock.id = "cityblock";
-  cityBlock.appendChild(city).className = "city";
-  cityBlock.appendChild(temp).className = "temperature";
-  cityBlock.appendChild(wind).className = "windspeed";
-  cityBlock.appendChild(uv).className = "uvindex";
-  city.textContent = "City";
-  temp.textContent = "temperature";
-  wind.textContent = "windspeed";
-  uv.textContent = "uv placeholder";
-}
-// https://stackoverflow.com/a/15643868/14244725 - uhh, put the variables inside the loop/function
-// creates the 5-day card html ==============================
-function dayCard () {
-  var dayTitle = document.querySelector("#daytitle");
-  dayTitle.textContent = "5-Day Forecast:";
-  for (i = 0; i < dayArray.length; i++) {
-    var dayS1 = document.createElement("article");
-    var dayS2 = document.createElement("article");
-    var dayH5 = document.createElement("h5");
-    var dayP1 = document.createElement("p");
-    cardTarget.after(dayS1);
-    dayS1.className ="card bg-primary";
-    dayS1.style = "width:10rem";
-    dayS1.dataset.day = "day"+[i+1];
-    dayS1.appendChild(dayS2).className ="card-body";
-    dayS2.appendChild(dayH5).className = "card-title";
-    dayS2.appendChild(dayP1).className = "card-text";
-  }
-}
+
+fetchButton.addEventListener('click', getData);
 
 recents();
